cbd250e18a0e86c0c2c25635d6738bad4324b6df
root <root@DESKTOP-66O118V.localdomain>
2020-03-19 19:18:15 +0200

    initiated celery to run tasks async

---
+++ e_shoppy/celery_app.py
@@ -0,0 +1,10 @@
+import os
+from celery import Celery
+
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "e_shoppy.settings")
+
+app = Celery("e_shoppy", broker="amqp://localhost//")
+
+app.config_from_object("django.conf:settings", namespace="CELERY")
+app.autodiscover_tasks()
